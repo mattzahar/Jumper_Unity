1cf65663c62480a0c5bd5adb7c1045d0f4f12d82
Luke Beatty <lcharlesb@Lukes-MacBook-Pro-3.local>
2020-04-24 12:18:42 -0400

    Animations for player.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,34 +6,44 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController2D controller;
+    public Animator animator;
     public float runSpeed = 40f;
     float horizontalMove = 0f;
     bool jump = false;
     bool crouch = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
-        } else if (Input.GetButtonDown("Crouch"))
+            animator.SetBool("isJumping", true);
+        }
+        if (Input.GetButtonDown("Crouch"))
         {
             crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+          crouch = false;
         }
+    }
+
+    public void OnLanding()
+    {
+      animator.SetBool("isJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+      animator.SetBool("isCrouching", isCrouching);
     }
 
     void FixedUpdate()
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
-        crouch = false;
     }
 }
